d841aacb73b8b2366d4de58dd89b502dc3afe872
Lehlohonolo.Mofokeng <lhmofokeng@yahoo.com>
2022-07-27 03:36:17 +0200

    Renamed project to 'Java for testers' and added some tests for division, multiplication and subtraction.

--- src/test/java/com/javafortesters/chapter003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chapter003myfirsttest/examples/MyFirstTest.java
@@ -4,18 +4,41 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyFirstTest {
-
+    int answer;
     //the method declaration
     //annotate the method with @Test do JUnit can recognise the test
 
     @Test
-    public void canAddTwoPlusTwo(){
-        int answer= 2+2;
-        Assert.assertEquals("The sum of 2 + 2 = 4",4,answer);
+    public void canAddTwoPlusTwo() {
+        answer = 2 + 2;
+        Assert.assertEquals("The sum of 2 + 2 = 4", 4, answer);
 
-        System.out.println(answer);
+        System.out.println("2+2 = " + answer);
 
+    }
+    @Test
+    public void canSubtractTwoMinusTwo() {
+        answer = 2 - 2;
+        Assert.assertEquals("Subtraction of 2-2=0", 0, answer);
 
+        System.out.println("2-2 = " + answer);
+
+    }
+
+    @Test
+    public void canDivideFourByTwo() {
+        answer = 4 / 2;
+        Assert.assertEquals("The division of 4/2 = 2", 2, answer);
+
+        System.out.println("4/2 = "+ answer);
+    }
+
+    @Test
+    public void canMultiplyTwoByTwo(){
+        answer= 2* 2;
+        Assert.assertEquals("The product of 2*2=4",4,answer);
+
+        System.out.println("2*2 = " + answer);
     }
 }
 
